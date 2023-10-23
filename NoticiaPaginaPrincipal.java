af3c5e0fb1c384714eb8e7b8dd61a61293ecec43
Xinyuuuuuuu <148193345+Xinyuuuuuuu@users.noreply.github.com>
2023-10-23 12:32:20 +0200

    Add files via upload

---
+++ NoticiaPaginaPrincipal.java
@@ -0,0 +1,184 @@
+package Reto1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
+
+
+public class NoticiaPaginaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NoticiaPaginaPrincipal frame = new NoticiaPaginaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NoticiaPaginaPrincipal() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("I:\\.shortcut-targets-by-id\\15dkCqPjbMIsgGF0lsmzUfUoErx1EL_qN\\G3-Reto1\\LMSG\\Fotos\\ideogram.jpeg"));
+		setAlwaysOnTop(true);
+		setTitle("Resultados Waterpolo Español");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1097, 707);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setExtendedState(this.MAXIMIZED_BOTH);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JButton btnPaginaPrincipal = new JButton("Página Principal");
+		menuBar.add(btnPaginaPrincipal);
+		btnPaginaPrincipal.setBackground(new Color(139, 0, 0));
+		btnPaginaPrincipal.setForeground(new Color(255, 255, 255));
+		btnPaginaPrincipal.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		
+		JButton btnResultados = new JButton("Resultados");
+		menuBar.add(btnResultados);
+		btnResultados.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnListaEquipos = new JButton("Lista Equipos");
+		menuBar.add(btnListaEquipos);
+		btnListaEquipos.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnCalendario = new JButton("Calendario");
+		menuBar.add(btnCalendario);
+		btnCalendario.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnClasificacion = new JButton("Clasificación");
+		menuBar.add(btnClasificacion);
+		btnClasificacion.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnSobreNosotros = new JButton("Sobre Nosotros");
+		menuBar.add(btnSobreNosotros);
+		btnSobreNosotros.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnFaq = new JButton("FAQ");
+		menuBar.add(btnFaq);
+		btnFaq.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnIdioma = new JButton("ES/EN");
+		menuBar.add(btnIdioma);
+		btnIdioma.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		JButton btnContacto = new JButton("Contacto");
+		menuBar.add(btnContacto);
+		btnContacto.setFont(new Font("Calibri", Font.BOLD, 20));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel containerPanelLeft = new JPanel(new BorderLayout());
+		int margenT = 50;
+		int margenL = 40;
+		int margenB = 70;
+		int margenR = 40;
+		containerPanelLeft.setBorder(BorderFactory.createEmptyBorder(margenT, margenL, margenB, margenR));
+		contentPane.add(containerPanelLeft, BorderLayout.WEST);
+		
+		JPanel newsPanel = new JPanel();
+	     newsPanel.setBackground(new Color(139, 0, 0));
+	     newsPanel.setLayout(new BoxLayout(newsPanel, BoxLayout.Y_AXIS));
+	     int margen = 10;
+	     newsPanel.setBorder(BorderFactory.createEmptyBorder(margen, margen, margen, margen));
+	     containerPanelLeft.add(newsPanel);
+	     
+	     JPanel centerPanel = new JPanel();
+	     centerPanel.setBackground(new Color(139, 0, 0));
+	     centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.X_AXIS));
+	     centerPanel.add(Box.createHorizontalGlue());
+	     JLabel lblNoticias = new JLabel("Noticias destacadas");
+	     lblNoticias.setHorizontalAlignment(SwingConstants.CENTER);
+	     lblNoticias.setForeground(new Color(255, 255, 255));
+	     lblNoticias.setFont(new Font("Calibri", Font.BOLD, 20));
+	     centerPanel.add(lblNoticias);
+	     centerPanel.add(Box.createHorizontalGlue()); 
+	     newsPanel.add(centerPanel);
+	     
+	     JPanel noticiaPanel = new JPanel();
+	     noticiaPanel.setLayout(new BoxLayout(noticiaPanel, BoxLayout.X_AXIS));
+	     noticiaPanel.add(Box.createHorizontalGlue());
+	     JLabel lblNoticiaWP = new JLabel("");
+	     lblNoticiaWP.setHorizontalAlignment(SwingConstants.CENTER);
+	     lblNoticiaWP.setIcon(new ImageIcon("G:\\.shortcut-targets-by-id\\15dkCqPjbMIsgGF0lsmzUfUoErx1EL_qN\\G3-Reto1\\Fotos\\Fotos Web Java\\noticia waterpolo.jpg"));
+	     noticiaPanel.add(lblNoticiaWP);
+	     noticiaPanel.add(Box.createHorizontalGlue());
+	     newsPanel.add(noticiaPanel);
+	     
+	     JPanel nuevaNoticiaPanel = new JPanel();
+	     nuevaNoticiaPanel.setLayout(new BoxLayout(nuevaNoticiaPanel, BoxLayout.X_AXIS));
+	     nuevaNoticiaPanel.add(Box.createHorizontalGlue());
+	     JLabel lblNuevaNoticiaWP = new JLabel("");
+	     lblNuevaNoticiaWP.setBackground(new Color(255, 255, 255));
+	     lblNuevaNoticiaWP.setHorizontalAlignment(SwingConstants.CENTER);
+	     lblNuevaNoticiaWP.setIcon(new ImageIcon("G:\\.shortcut-targets-by-id\\15dkCqPjbMIsgGF0lsmzUfUoErx1EL_qN\\G3-Reto1\\Fotos\\Fotos Web Java\\noticia nueva waterpolo.jpg"));
+	     nuevaNoticiaPanel.add(lblNuevaNoticiaWP);
+	     nuevaNoticiaPanel.add(Box.createHorizontalGlue());
+	     newsPanel.add(nuevaNoticiaPanel);
+		
+	     JPanel containerPanelRight = new JPanel(new BorderLayout());
+		containerPanelRight.setBorder(BorderFactory.createEmptyBorder(margenT, margenL, margenB, margenR));
+		contentPane.add(containerPanelRight, BorderLayout.EAST);
+		
+		JPanel matchesPanel = new JPanel();
+		matchesPanel.setBackground(new Color(139, 0, 0));
+		matchesPanel.setLayout(new BoxLayout(matchesPanel, BoxLayout.Y_AXIS));
+		matchesPanel.setBorder(BorderFactory.createEmptyBorder(margen, margen, margen, margen));
+		containerPanelRight.add(matchesPanel);
+	     
+	     JPanel centerPanelPartidos = new JPanel();
+	     centerPanelPartidos.setBackground(new Color(139, 0, 0));
+	     centerPanelPartidos.setLayout(new BoxLayout(centerPanelPartidos, BoxLayout.X_AXIS));
+	     centerPanelPartidos.add(Box.createHorizontalGlue());
+	    	JLabel lblProximosPartidos = new JLabel("Próximos partidos");
+	    	lblProximosPartidos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProximosPartidos.setForeground(new Color(255, 255, 255));
+		lblProximosPartidos.setFont(new Font("Calibri", Font.BOLD, 20));
+		centerPanelPartidos.add(lblProximosPartidos);
+		centerPanelPartidos.add(Box.createHorizontalGlue()); 
+	     matchesPanel.add(centerPanelPartidos);
+	    
+		JPanel partidosPanel = new JPanel();
+		partidosPanel.setLayout(new BoxLayout(partidosPanel, BoxLayout.X_AXIS));
+		partidosPanel.add(Box.createHorizontalGlue());
+		JLabel lblPartidos = new JLabel("");
+		lblPartidos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPartidos.setIcon(new ImageIcon("G:\\.shortcut-targets-by-id\\15dkCqPjbMIsgGF0lsmzUfUoErx1EL_qN\\G3-Reto1\\Fotos\\JORNADA ORDENADA.png"));
+		noticiaPanel.add(Box.createHorizontalGlue());
+		partidosPanel.add(lblPartidos);
+	     matchesPanel.add(partidosPanel);
+	}
+
+	}
+
