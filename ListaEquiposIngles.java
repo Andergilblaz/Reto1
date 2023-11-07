79cad2ff400f00329683d8d6ceda86954ea33147
lamarireal <148193431+lamarireal@users.noreply.github.com>
2023-11-07 12:35:05 +0100

    Update ListaEquiposIngles.java

--- ListaEquiposIngles.java
+++ ListaEquiposIngles.java
@@ -111,6 +111,7 @@ public class ListaEquiposIngles extends JFrame {
         tableHeader.setFont(new Font("Calibri", Font.BOLD, 20));
         tableHeader.setBackground(Color.WHITE);
         tableHeader.setForeground(new Color(35, 33, 156));
+	tableHeader.setReorderingAllowed(false);
  
         contentPane.setLayout(new BorderLayout(0, 0));
 		
