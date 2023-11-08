fda2d1e5c9415e6c86899fa2ed82505cd4eba93a
alaiinluquee <148193334+alaiinluquee@users.noreply.github.com>
2023-11-08 12:48:13 +0100

    Add files via upload

--- CalendarioING.java
+++ CalendarioING.java
@@ -55,176 +55,20 @@ public class CalendarioING extends JFrame {
 		setForeground(new Color(139, 0, 0));
 		setIconImage(Toolkit.getDefaultToolkit().getImage("I:\\.shortcut-targets-by-id\\15dkCqPjbMIsgGF0lsmzUfUoErx1EL_qN\\G3-Reto1\\LMSG\\Fotos\\ideogram.jpeg"));
 		setAlwaysOnTop(true);
-		setTitle("Resultados Waterpolo Español");
+		setTitle("Spanish Water Polo Results");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 282, 530);
+		setBounds(100, 100, 899, 569);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.setExtendedState(Frame.MAXIMIZED_BOTH);
-		
+		setLocationRelativeTo(null);
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBorderPainted(false);
 		menuBar.setBackground(new Color(139, 0, 0));
 		menuBar.setFont(new Font("Calibri", Font.BOLD, 20));
 		setJMenuBar(menuBar);
 		
-		
-		JButton btnPaginaPrincipal = new JButton("Home Page");
-		menuBar.add(btnPaginaPrincipal);
-		btnPaginaPrincipal.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnPaginaPrincipal.setBackground(new Color(139, 0, 0));
-		btnPaginaPrincipal.setForeground(new Color(255, 255, 255));
-		btnPaginaPrincipal.setFont(new Font("Calibri", Font.BOLD, 20));
-		
-		
-		JButton btnResultados = new JButton("Results");
-		menuBar.add(btnResultados);
-		btnResultados.setForeground(new Color(255, 255, 255));
-		btnResultados.setBackground(new Color(139, 0, 0));
-		btnResultados.setFont(new Font("Calibri", Font.BOLD, 20));
-		
-		JMenu mnCalendario = new JMenu("Schedule");
-		mnCalendario.setFont(new Font("Calibri", Font.BOLD, 20));
-		mnCalendario.setForeground(new Color(255, 255, 255));
-		mnCalendario.setBackground(new Color(139, 0, 0));
-		menuBar.add(mnCalendario);
-		
-		JMenuItem mntmCalendario = new JMenuItem("Schedule");
-		mntmCalendario.setBackground(new Color(139, 0, 0));
-		mntmCalendario.setForeground(new Color(255, 255, 255));
-		mnCalendario.add(mntmCalendario);
-		
-		JMenuItem mntmJornada1 = new JMenuItem("Week 1");
-		mntmJornada1.setForeground(new Color(255, 255, 255));
-		mntmJornada1.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada1);
-		
-		JMenuItem mntmJornada2 = new JMenuItem("Week 2");
-		mntmJornada2.setForeground(new Color(255, 255, 255));
-		mntmJornada2.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada2);
-		
-		JMenuItem mntmJornada3 = new JMenuItem("Week 3");
-		mntmJornada3.setForeground(new Color(255, 255, 255));
-		mntmJornada3.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada3);
-		
-		JMenuItem mntmJornada4 = new JMenuItem("Week 4");
-		mntmJornada4.setBackground(new Color(139, 0, 0));
-		mntmJornada4.setForeground(new Color(255, 255, 255));
-		mnCalendario.add(mntmJornada4);
-		
-		JMenuItem mntmJornada5 = new JMenuItem("Week 5");
-		mntmJornada5.setBackground(new Color(139, 0, 0));
-		mntmJornada5.setForeground(new Color(255, 255, 255));
-		mnCalendario.add(mntmJornada5);
-		
-		JMenuItem mntmJornada6 = new JMenuItem("Week 6");
-		mntmJornada6.setForeground(new Color(255, 255, 255));
-		mntmJornada6.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada6);
-		
-		JMenuItem mntmJornada7 = new JMenuItem("Week 7");
-		mntmJornada7.setForeground(new Color(255, 255, 255));
-		mntmJornada7.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada7);
-		
-		JMenuItem mntmJornada8 = new JMenuItem("Week 8");
-		mntmJornada8.setForeground(new Color(255, 255, 255));
-		mntmJornada8.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada8);
-		
-		JMenuItem mntmJornada9 = new JMenuItem("Week 9");
-		mntmJornada9.setForeground(new Color(255, 255, 255));
-		mntmJornada9.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada9);
-		
-		JMenuItem mntmJornada10 = new JMenuItem("Week 10");
-		mntmJornada10.setForeground(new Color(255, 255, 255));
-		mntmJornada10.setBackground(new Color(139, 0, 0));
-		mnCalendario.add(mntmJornada10);
-		
-		JButton btnListaEquipos = new JButton("Team List");
-		btnListaEquipos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		menuBar.add(btnListaEquipos);
-		btnListaEquipos.setBackground(new Color(139, 0, 0));
-		btnListaEquipos.setForeground(new Color(255, 255, 255));
-		btnListaEquipos.setFont(new Font("Calibri", Font.BOLD, 20));
-		
-		JButton btnClasificacion = new JButton("League Table");
-		menuBar.add(btnClasificacion);
-		btnClasificacion.setForeground(new Color(255, 255, 255));
-		btnClasificacion.setBackground(new Color(139, 0, 0));
-		btnClasificacion.setFont(new Font("Calibri", Font.BOLD, 20));
-		
-		JMenu mnSobreNosotros = new JMenu("About us");
-		mnSobreNosotros.setFont(new Font("Calibri", Font.BOLD, 20));
-		mnSobreNosotros.setForeground(new Color(255, 255, 255));
-		mnSobreNosotros.setBackground(new Color(139, 0, 0));
-		menuBar.add(mnSobreNosotros);
-		
-		JMenuItem mntmQuienesSomos = new JMenuItem("History");
-		mntmQuienesSomos.setForeground(new Color(255, 255, 255));
-		mntmQuienesSomos.setBackground(new Color(139, 0, 0));
-		mnSobreNosotros.add(mntmQuienesSomos);
-		
-		JMenuItem mntmComoHaSurgido = new JMenuItem("How did this website come about?");
-		mntmComoHaSurgido.setForeground(new Color(255, 255, 255));
-		mntmComoHaSurgido.setBackground(new Color(139, 0, 0));
-		mnSobreNosotros.add(mntmComoHaSurgido);
-		
-		JMenu mnFAQ = new JMenu("FAQ");
-		mnFAQ.setFont(new Font("Calibri", Font.BOLD, 20));
-		mnFAQ.setBackground(new Color(139, 0, 0));
-		mnFAQ.setForeground(new Color(255, 255, 255));
-		menuBar.add(mnFAQ);
-		
-		JMenuItem mntmComoFunciona = new JMenuItem("How does this website work?");
-		mntmComoFunciona.setForeground(new Color(255, 255, 255));
-		mntmComoFunciona.setBackground(new Color(139, 0, 0));
-		mnFAQ.add(mntmComoFunciona);
-		
-		JMenuItem mntmQueHago = new JMenuItem("What if there is an error?");
-		mntmQueHago.setBackground(new Color(139, 0, 0));
-		mntmQueHago.setForeground(new Color(255, 255, 255));
-		mnFAQ.add(mntmQueHago);
-		
 		JSeparator separator_1 = new JSeparator();
 		menuBar.add(separator_1);
-		
-		
-		JButton btnInicioSesion = new JButton("Log In");
-		menuBar.add(btnInicioSesion);
-		btnInicioSesion.setFont(new Font("Calibri", Font.BOLD, 20));
-		btnInicioSesion.setBackground(new Color(139, 0, 0));
-		btnInicioSesion.setForeground(new Color(255, 255, 255));
-		
-		JMenu mnIdioma = new JMenu("ES/EN");
-		mnIdioma.setForeground(new Color(255, 255, 255));
-		mnIdioma.setBackground(new Color(139, 0, 0));
-		menuBar.add(mnIdioma);
-		
-		JMenuItem mntmEspañol = new JMenuItem("Español");
-		mntmEspañol.setForeground(new Color(255, 255, 255));
-		mntmEspañol.setBackground(new Color(139, 0, 0));
-		mnIdioma.add(mntmEspañol);
-		
-		JMenuItem mntmIngles = new JMenuItem("English");
-		mntmIngles.setForeground(new Color(255, 255, 255));
-		mntmIngles.setBackground(new Color(139, 0, 0));
-		mnIdioma.add(mntmIngles);
-		
-		JButton btnContacto = new JButton("Contact us");
-		menuBar.add(btnContacto);
-		btnContacto.setForeground(new Color(255, 255, 255));
-		btnContacto.setBackground(new Color(139, 0, 0));
-		btnContacto.setFont(new Font("Calibri", Font.BOLD, 20));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
