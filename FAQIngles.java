386dc8be530eeb9429c320cbab0457fe200ab453
Andergilblaz <148193412+Andergilblaz@users.noreply.github.com>
2023-11-10 10:18:37 +0100

    Update FAQIngles.java

--- FAQIngles.java
+++ FAQIngles.java
@@ -45,6 +45,7 @@ public class FAQIngles extends JFrame {
 		setBounds(100, 100, 978, 602);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -79,28 +80,28 @@ public class FAQIngles extends JFrame {
         lblNewLabel_2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); 
         panel_1.add(lblNewLabel_2);
         
-        JLabel lblNewLabel_3 = new JLabel("<html>3: How to change the language of the application?<br>- There are currently 2 languages available to use the application: Spanish, English. To change <br> the language, use the \"Es/En\" menu.</html>");
+        JLabel lblNewLabel_3 = new JLabel("<html>3: How can I change the language of the application?<br>- There are currently 2 languages available to use the application: Spanish, English. To change <br> the language, use the \"Es/En\" menu.</html>");
         lblNewLabel_3.setFont(new Font("Calibri", Font.BOLD, 20));
         lblNewLabel_3.setVerticalAlignment(SwingConstants.TOP);
         lblNewLabel_3.setHorizontalAlignment(SwingConstants.LEFT);
         lblNewLabel_3.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         panel_1.add(lblNewLabel_3);
         
-        JLabel lblNewLabel_4 = new JLabel("<html>4: What should I do if the application is not working correctly?<br>- In this case, we recommend restarting the application, and also sending us a message through <br>the \"Contact\" menu, describing the problem.</html>");
+        JLabel lblNewLabel_4 = new JLabel("<html>4: What should I do if the application is not working correctly?<br>- In that case, we recommend restarting the application, and also sending us a message through <br>the \"Contact\" menu, describing the problem.</html>");
         lblNewLabel_4.setFont(new Font("Calibri", Font.BOLD, 20));
         lblNewLabel_4.setVerticalAlignment(SwingConstants.TOP);
         lblNewLabel_4.setHorizontalAlignment(SwingConstants.LEFT);
         lblNewLabel_4.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         panel_1.add(lblNewLabel_4);
         
-        JLabel lblNewLabel_5 = new JLabel("<html>5: How to contact you?<br>- If you haven't found the answer to your question, you can always send us a message using <br>the \"Contact\" menu.</html>");
+        JLabel lblNewLabel_5 = new JLabel("<html>5: How to contact us?<br>- If you haven't found the answer to your question, you can always send us a message using <br>the \"Contact\" menu.</html>");
         lblNewLabel_5.setFont(new Font("Calibri", Font.BOLD, 20));
         lblNewLabel_5.setVerticalAlignment(SwingConstants.TOP);
         lblNewLabel_5.setHorizontalAlignment(SwingConstants.LEFT);
         lblNewLabel_5.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         panel_1.add(lblNewLabel_5);
         
-        JLabel lblNewLabel_6 = new JLabel("<html>6: How do I log in to the publisher account?<br>- If you are an editor, developer and/or administrator of this project, you must have your own username <br>and personal password.<br>To log in to your account, use the \"Login menu \", then enter your personal name and password.</html>");
+        JLabel lblNewLabel_6 = new JLabel("<html>6: How do I log in to the editor account?<br>- If you are an editor, developer and/or administrator of this project, you must have your own username <br>and personal password.<br>To log in to your account, use the \"Login menu \", then enter your personal name and password.</html>");
         lblNewLabel_6.setFont(new Font("Calibri", Font.BOLD, 20));
         lblNewLabel_6.setVerticalAlignment(SwingConstants.TOP);
         lblNewLabel_6.setHorizontalAlignment(SwingConstants.LEFT);
