3cd32ce97a81562b9f21a1fa406b3bf24fbe4f39
lamarireal <148193431+lamarireal@users.noreply.github.com>
2023-11-13 08:59:40 +0100

    Update Calendario.java

--- Calendario.java
+++ Calendario.java
@@ -42,7 +42,7 @@ public class Calendario extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src\\Reto1\\fotos\\LogoWaterpoloPequeña.png"));
 		setAlwaysOnTop(true);
 		setTitle("Resultados Waterpolo Español");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 978, 585);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
