f8f48a2c795154cc86abdc1ad73b070cd3d178fd
lamarireal <148193431+lamarireal@users.noreply.github.com>
2023-11-13 09:46:04 +0100

    Update FAQ.java

--- FAQ.java
+++ FAQ.java
@@ -41,7 +41,7 @@ public class FAQ extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src\\Reto1\\fotos\\LogoWaterpoloPequeña.png"));
 		setAlwaysOnTop(true);
 		setTitle("FAQ Waterpolo Español");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 978, 602);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
